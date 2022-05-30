9cc23bc61da704dbceea3e781d0f404be631b773
roneilbansas5222 <roneilbansas5222@gmail.com>
2022-05-30 13:24:29 -0700

    reanimated test

--- App.js
+++ App.js
@@ -1,27 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Chat from "./screens/Chat";
 
-const Stack = createNativeStackNavigator();
-
-function ChatStack() {
+export default App = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Chat" component={Chat} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
   );
-}
+};
 
-function RootNavigator() {
-  return (
-    <NavigationContainer>
-      <ChatStack />
-    </NavigationContainer>
-  );
-}
-
-export default function App() {
-  return <RootNavigator />;
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
