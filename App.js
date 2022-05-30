7347b2fb0c592adeac379c98fb1f1bb683e50a5e
roneilbansas5222 <roneilbansas5222@gmail.com>
2022-05-30 14:40:15 -0700

    reanimated tutorial-01

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSpring,
+  withRepeat,
+} from "react-native-reanimated";
 
-export default App = () => {
+const SIZE = 100.0;
+
+export default function App() {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [
+        { scale: scale.value },
+        { rotate: `${progress.value * 2 * Math.PI}rad` },
+      ],
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), -1, true);
+    scale.value = withRepeat(withSpring(0.5), -1, true);
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      <Animated.View
+        style={[
+          { height: SIZE, width: SIZE, backgroundColor: "orangered" },
+          reanimatedStyle,
+        ]}
+      />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
