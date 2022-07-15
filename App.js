b057f9f7bc628d5a915ccf9c887a3976c38b9cd7
Miracle5222 <roneilgungobbansas5222@gmail.com>
2022-07-15 05:01:06 -0700

    m

--- App.js
+++ App.js
@@ -1,50 +1,23 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
-import React, { useEffect } from "react";
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-  withSpring,
-  withRepeat,
-} from "react-native-reanimated";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import TodoHeader from "./app/components/TodoHeader";
 
-const SIZE = 100.0;
+const App = () => {
+  const RootApp = () => {
+    return (
+      <View>
+        <TodoHeader />
+      </View>
+    );
+  };
 
-export default function App() {
-  const progress = useSharedValue(1);
-  const scale = useSharedValue(2);
-
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: progress.value,
-      borderRadius: (progress.value * SIZE) / 2,
-      transform: [
-        { scale: scale.value },
-        { rotate: `${progress.value * 2 * Math.PI}rad` },
-      ],
-    };
-  }, []);
-
-  useEffect(() => {
-    progress.value = withRepeat(withSpring(0.5), -1, true);
-    scale.value = withRepeat(withSpring(0.5), -1, true);
-  }, []);
   return (
-    <View style={styles.container}>
-      <Animated.View
-        style={[
-          { height: SIZE, width: SIZE, backgroundColor: "orangered" },
-          reanimatedStyle,
-        ]}
-      />
+    <View>
+      <RootApp />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
