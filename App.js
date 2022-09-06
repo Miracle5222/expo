3b0b8f3525d059819a25ca8bf9c8628eca9e4f81
Miracle5222 <roneilgungobbansas5222@gmail.com>
2022-09-05 23:11:09 -0700

    first commit axios

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost/capstone/")
+
+      .then(function (response) {
+        console.log(response);
+        setState(response.data.data);
+        console.log(state.title);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [state]);
+
+  // useEffect(() => {
+  //   let Data = {
+  //     name: "Roneil",
+  //   };
+  //   let headers = {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   };
+  //   fetch("http://localhost/capstone/response", {
+  //     method: "POST",
+  //     header: headers,
+  //     body: JSON.stringify(Data),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((response) => alert(response[0].Message))
+  //     .catch((error) => alert("Error Occured" + error));
+  // }, []);
   return (
     <View>
-      <Text>App</Text>
+      <Text>{state.title}</Text>
     </View>
   );
 };
